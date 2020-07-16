bc14c1e23f2250fea6773c1e497914647768f5fc
LloydLion <LloydLion2211@gmail.com>
2020-07-16 13:33:37 +0300

    OpenServer Directory Action

---
+++ MineCraft Server Maneger/Actions/OpenServerDirectoryAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineCraft_Server_Maneger.Actions
+{
+	class OpenServerDirectoryAction : ActionBase
+	{
+		public override string ActionName => "other.open_server_directory";
+
+		public override void Handler()
+		{
+			Process.Start("explorer.exe", Static.Path.data);
+		}
+
+		public override bool IsEnabled() => true;
+	}
+}
